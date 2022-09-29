76cbc4b3e2f2a30bd638d169d3ab6506f4543f09
Ignacio <ig.dinamarca@duocuc.cl>
2022-09-28 22:12:47 -0300

    metodo get y post

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -12,7 +12,7 @@ export class ApiService {
       'Access-Control-Allow-Origin': '*'
     })
   } 
-  
+
   // Se establece la base url del API a consumir
 
   apiURL = 'https://jsonplaceholder.typicode.com'; 
@@ -20,3 +20,16 @@ export class ApiService {
   // Se declara la variable http de tipo HttpClient 
   
   constructor(private http:HttpClient) { }
+
+  getPosts(id):Observable<any>{
+     return this.http.get(this.apiURL+'/posts/').pipe(
+       retry(3) 
+     ); 
+  }
+
+  createPost(post):Observable<any>{
+     return this.http.post(this.apiURL+'/posts',post,this.httpOptions)
+      .pipe(
+         retry(3) 
+      ); 
+  }
